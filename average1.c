f220d17a238cb1310bb783c0cac2dd70ca9bc77e
SHIVAMVALAND <140816892+SHIVAMVALAND@users.noreply.github.com>
2023-09-27 21:37:31 +0530

    Add files via upload

---
+++ average1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,avg;
+    printf("enter 1st number: ");
+    scanf("%d",&a);
+
+    printf("enter 2nd number: ");
+    scanf("%d",&b);
+
+    avg=(a+b)/2;
+
+    printf("%d",avg);
+}
